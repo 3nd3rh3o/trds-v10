c23be4cb08bd16c941d7ea2ba91eb35c4cf0fb61
3nd3rh3o <theenderman3333@gmail.com>
2026-01-04 14:51:25 +0100

    Add new panel and screen components to TARDIS console and implement ArsScreen GUI
    - Extended the TARDIS console geometry by adding a new panel (panel3) with various display elements including cubes and screens.
    - Introduced the ArsScreen class to handle the architectural reconfiguration screen, featuring a "Done" button for user interaction.

---
+++ src/main/java/ender/dwmod/tardis/systems/architecturalReconfiguration/ArsScreen.java
@@ -0,0 +1,30 @@
+package ender.dwmod.tardis.systems.architecturalReconfiguration;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class ArsScreen extends Screen {
+    public ArsScreen(Component title)
+    {
+        super(title);
+    }
+
+    @Override
+    protected void init() {
+        Button exitButton = Button.builder(Component.nullToEmpty("Done"), btn ->
+        {
+            this.onClose();
+        }).bounds(this.width / 2 - 50, this.height - 30, 100, 20).build();
+        this.addRenderableWidget(exitButton);
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+    }
+
+    
+    
+}
